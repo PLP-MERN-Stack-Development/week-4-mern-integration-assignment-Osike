540d7ad3236791fafbb178541d210e370dc97d50
Osike <shadyabacus@gmail.com>
2025-07-09 13:45:52 +0300

    initial commit

---
+++ mern-blog/server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Blog API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
